a847086909bf1c385578d9abc88149dd9eecc5b0
manish <officialmanishkr98@gmail.com>
2022-04-09 22:32:37 +0530

    commit

---
+++ L-1/main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class main {
+    public static void main(String args[]) 
+    {
+        //Scanner scn = new Scanner(System.in);
+        int  = scn.nextInt();
+
+    }
+
+}
